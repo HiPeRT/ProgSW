153c583df8d405914cbe251f4b32d672568cb15f
Paolo Burgio <paolo.burgio@gmail.com>
2024-05-27 14:20:24 +0200

    Modified Basic C# examples

--- Code/DotNetBasics/ConsoleApp/Program.cs
+++ Code/DotNetBasics/ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ using System.Dynamic;
 // Same as "package" in Java
 namespace DotNetBasics.ConsoleApp
 {
+
     /// <summary>
     /// This is the entry point of a console application
     /// </summary>
@@ -16,84 +17,84 @@ namespace DotNetBasics.ConsoleApp
         {
             // 1. Referencing a class from another (shared code) project
 
-            //MyClass myObj = new MyClass();
+            NewClass myObj = new NewClass();
 
-            //int multiplyRes = myObj.MultiplyByTwo(11);
-            //int divideRes = myObj.DivideByTwo(10);
+            int multiplyRes = myObj.MultiplyByTwo(11);
+            int divideRes = myObj.DivideByTwo(10);
 
-            //Console.WriteLine($"Result is { multiplyRes } - { divideRes }");
+            Console.WriteLine($"Result is {multiplyRes} - {divideRes}");
 
             // 2. Boxing and Unboxing
 
-            //int a = 11;
-            //object obj = a;
+            int a = 11;
+            object obj = a;
 
-            //Console.WriteLine($"Obj is { obj }");
+            Console.WriteLine($"Obj is {obj}");
 
-            //// Unbox the value
-            //int b = (int)obj;
+            // Unbox the value
+            int b = (int)obj;
 
-            //object[] myArr = { "ciao", 2, 'c', 456.781 };
-            //foreach(object o in myArr)
-            //    Console.WriteLine(o);
+            object[] myArr = { "ciao", 2, 'c', 456.781 };
+            foreach (object o in myArr)
+                Console.WriteLine(o);
 
-            //// Print only integers
-            //foreach(object o in myArr )
-            //    if(o is int myInt)
-            //        Console.WriteLine(myInt);
+            // Print only integers
+            foreach (object o in myArr)
+                if (o is int myInt)
+                    Console.WriteLine(myInt);
 
             // 3. Generics (compiler infers data type)
 
-            //var myVar = 5;
-            //Console.WriteLine($"myVar is {myVar}");
+            var myVar = 5;
+            Console.WriteLine($"myVar is {myVar}");
 
-            //// 4. Nullables
-            //MyClass?[] maybeObjects =
-            //{
-            //    MyClass.MaybeReturnObject(0), // This is null
-            //    MyClass.MaybeReturnObject(1), // This is not null
-            //};
+            // 4. Nullables
+            MyClass?[] maybeObjects =
+            {
+                MyClass.MaybeReturnObject(0), // This is null
+                MyClass.MaybeReturnObject(1), // This is not null
+            };
 
-            //foreach (var o in maybeObjects)
-            //    if (o is not null)
-            //        Console.WriteLine($"Result on non-null obj is {o.MultiplyByTwo(4)}");
+            foreach (var o in maybeObjects)
+                if (o is not null)
+                    Console.WriteLine($"Result on non-null obj is {o.MultiplyByTwo(4)}");
 
             // 5. Anonymous and Dynamics
 
-            //var myAnonymous = new
-            //{
-            //    intField = 4,
-            //    doubleField = 4.527,
-            //    quasiIntField = (int) 4.6
-            //};
+            var myAnonymous = new
+            {
+                intField = 4,
+                doubleField = 4.527,
+                quasiIntField = (int)4.6
+            };
 
-            //// This won't work (as the object would be identified as "ExpandoObject")
-            ////var myDynamic = new ExpandoObject();
+            // This won't work (as the object would be identified as "ExpandoObject")
+            //var myDynamic = new ExpandoObject();
 
-            //// This works, because the "dynamic" keyword/datatype enables
-            //// lazy type check by the compiler
-            //dynamic myDynamic = new ExpandoObject();
+            // This works, because the "dynamic" keyword/datatype enables
+            // lazy type check by the compiler
+            dynamic myDynamic = new ExpandoObject();
 
-            //myDynamic.SomeField = 5;
-            //if (a < 10)
-            //    myDynamic.SomeOtherField = "ciao";
+            myDynamic.SomeField = 5;
+            if (a < 10)
+                myDynamic.SomeOtherField = "ciao";
 
-            //Console.WriteLine($"SomeField is {myDynamic.SomeField}");
-            //// Typecheck is not performed, so this causes a RUNTIME error
+            Console.WriteLine($"SomeField is {myDynamic.SomeField}");
+            // Typecheck is not performed, so this causes a RUNTIME error
 
-            //try
-            //{
-            //    Console.WriteLine($"SomeField is {myDynamic.NonExistentFiel}");
-            //}
-            //catch (RuntimeBinderException)
-            ////catch (RuntimeBinderException rbe)
-            //{
-            //    // BTW: NEVER LEAVE A CATCH EXPRESSION EMPTY
-            //}
-            //catch
-            //{
-            //    Console.WriteLine("A Generic Exception occurred");
-            //}
+            try
+            {
+                Console.WriteLine($"SomeField is {myDynamic.NonExistentFiel}");
+            }
+            catch (RuntimeBinderException)
+            //catch (RuntimeBinderException rbe)
+            {
+                // BTW: NEVER LEAVE A CATCH EXPRESSION EMPTY
+            }
+            catch
+            {
+                Console.WriteLine("A Generic Exception occurred");
+            }
 
             // 6. Getters and setters
             //var p = new Person();
---
+++ Code/DotNetBasics/MySharedProject/NewClass.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetBasics.MySharedProject
+{
+    public class NewClass
+    {
+        public int MultiplyByTwo(int x)
+        {
+            return x * 2;
+        }
+
+        public int DivideByTwo(int x)
+        {
+            return x / 2;
+        }
+    }
+}
