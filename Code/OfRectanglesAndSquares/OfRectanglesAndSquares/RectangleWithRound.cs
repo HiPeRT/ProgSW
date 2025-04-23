43e5858fd222427a8ab0eaf254fb741df32a5841
Paolo Burgio <paolo.burgio@gmail.com>
2025-04-23 16:09:26 +0200

    Implemented RectangleWithRound in C# to align with Java exampleCompleted implementation of Java example of inheritance

---
+++ Code/OfRectanglesAndSquares/OfRectanglesAndSquares/RectangleWithRound.cs
@@ -0,0 +1,22 @@
+﻿namespace OfRectanglesAndSquares
+{
+    /// <summary>
+    /// This class rounds Rectangle height and width on sets to the ceiling integer
+    /// </summary>
+    public class RectangleWithRound : Rectangle
+    {
+        public RectangleWithRound(double height, double width) : base(height, width)
+        {
+        }
+
+        public override void SetHeight(double height)
+        {
+            base.SetHeight(Math.Ceiling(height));
+        }
+
+        public override void SetWidth(double width)
+        {
+            base.SetWidth(Math.Ceiling(width));
+        }
+    }
+}
