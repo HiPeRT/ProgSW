f628d15820522ad03e449ba31870e05412552db8
Paolo Burgio <paolo.burgio@gmail.com>
2026-04-22 14:11:49 +0100

    Added Singleton example

---
+++ Code/Singleton.Java/src/singleton/MainClass.java
@@ -0,0 +1,21 @@
+package singleton;
+
+/**
+ * This class is not thread safe
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		SingletonClass obj0 = SingletonClass.getInstance();
+		SingletonClass obj1 = SingletonClass.getInstance();
+		SingletonClass obj2 = SingletonClass.getInstance();
+		SingletonClass obj3 = SingletonClass.getInstance();
+		SingletonClass obj4 = SingletonClass.getInstance();
+
+		obj0.printValAndInc(); // This should print 'Val is 0'
+		obj1.printValAndInc(); // This should print 'Val is 1'
+		obj2.printValAndInc(); // This should print 'Val is 2'
+		obj3.printValAndInc(); // This should print 'Val is 3'
+		obj4.printValAndInc(); // This should print 'Val is 4'
+	}
+}
---
+++ Code/Singleton.Java/src/singleton/SingletonClass.java
@@ -0,0 +1,27 @@
+package singleton;
+
+/**
+ * This class implements a singleton pattern
+ */
+public class SingletonClass {
+	// Used to check whether we are accessing the singleton object
+	private int _val = 0;
+	
+	private static SingletonClass _Instance = null;
+	
+	private SingletonClass() {
+		
+	}
+	
+	public void printValAndInc() {
+		System.out.println("Val is " + _val);
+		_val++;
+	}
+	
+	public static SingletonClass getInstance() {
+		if(_Instance == null)
+			_Instance = new SingletonClass();
+			
+		return _Instance;
+	}
+}
